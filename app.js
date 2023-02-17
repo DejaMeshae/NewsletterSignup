6f748d93e36edf149051a75a496bcd4def3bba45
DejaMeshae <daemeshae@yahoo.com>
2023-02-16 19:43:37 -0600

    first commit

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-const { STATUS_CODES } = require("http");
-const { Stats } = require("fs");
 
 //creating express = app
 const app = express();
@@ -23,52 +21,59 @@ app.post("/", function(req, res){
     const email = req.body.email;
 
     const data = {
-        members: [
-        {
-            email_address: email, 
-            status: "subscribed",
-            merge_fields:{
-                FNAME: firstName,
-                LNAME: lastName
-            } 
-        }
-      ]
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
     };
 
-    const jsonData = JSON.stringify(data)
+    const jsonData = JSON.stringify(data);
 
     const url = "https://us11.api.mailchimp.com/3.0/lists/25bcba3ca0";
 
     const options = {
         method: "POST",
         auth: "deja1:81e55c338ef6ca1678d46ff74bf26e70-us11"
-    }
+    } 
 
     const request = https.request(url, options, function(response) {
-        //if else statement to let users know it was successful
-        if (response.statusCode === 200) {
+
+        if(response.statusCode === 200){
             res.sendFile(__dirname + "/success.html");
-        } else {
-            res.sendFile(__dirname + "/failure.html");
-        }     
-        
+        }   else {
+           res.sendFile(__dirname + "/failure.html"); 
+        }
+
         response.on("data", function(data){
-            //console.log(JSON.parse(data));
-        })
-    })
+            console.log(JSON.parse(data));
+        });
+    });
 
     request.write(jsonData);
     request.end();
-    
+
 });
 
-app.post("/failure", function(req, res) {
+//what happens when the user hits try again button, get sent back to home page
+app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+
+//what happens when the user hits sign up another button, get sent back to home page
+app.post("/success", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT ||3000, function() {
     //console.log("Server Working port 3000");
-    console.log("deployed on Heroku or listen to 30001")
+    console.log("deployed on Heroku or listen to 3000")
 });
 
 //api key
@@ -76,3 +81,4 @@ app.listen(process.env.PORT || 3000, function() {
 
 //list id
 //25bcba3ca0
+
